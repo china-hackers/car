6c8318a0859d28632378f6fe91dc4a4732e8d6f8
xiongxiaotao <xxtao110@163.com>
2018-09-11 21:56:04 +0800

    后台提交

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,1 +1,27 @@
-Hello World!
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>ayk-admin</title>
+    <?php if(YII_DEBUG):?>
+    <?php else:?>
+        <link href="/static/admin/css/app.css" rel="stylesheet">
+    <?php endif;?>
+    
+</head>
+
+<body module=live>
+    <div id=app></div>
+    <?php if(YII_DEBUG):?>
+    <?php else:?>
+        <script type="text/javascript" src="./static/admin/js/manifest.js"> </script>
+        <script type="text/javascript" src="./static/admin/js/vendor.js"> </script>
+        <script type="text/javascript" src="./static/admin/js/app.js"> </script>
+    <?php endif;?>
+</body>
+
+</html>
