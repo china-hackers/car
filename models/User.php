0182de3a99a3971f337a6797e484c246e90153d9
Leonidax <wap@iamlk.cn>
2018-10-14 21:45:19 +0800

    修复

--- models/User.php
+++ models/User.php
@@ -18,8 +18,10 @@ use Yii;
  * @property int $is_checked
  * @property int $business_id 所属车商
  * @property int $is_manager 是否管理员
- * @property int $id_card 身份证号
+ * @property string $id_card 身份证号
  * @property int $rid 推荐人ID
+ *
+ * @property Business $business
  */
 class User extends \yii\db\ActiveRecord
 {
@@ -38,12 +40,14 @@ class User extends \yii\db\ActiveRecord
     {
         return [
             [['avator'], 'required'],
-            [['city_id', 'is_checked', 'business_id', 'is_manager', 'id_card', 'rid'], 'integer'],
+            [['city_id', 'is_checked', 'business_id', 'is_manager', 'rid'], 'integer'],
             [['nick'], 'string', 'max' => 50],
             [['avator', 'open_id'], 'string', 'max' => 250],
             [['name'], 'string', 'max' => 30],
             [['sex'], 'string', 'max' => 2],
             [['phone'], 'string', 'max' => 16],
+            [['id_card'], 'string', 'max' => 20],
+            [['business_id'], 'exist', 'skipOnError' => true, 'targetClass' => Business::className(), 'targetAttribute' => ['business_id' => 'id']],
         ];
     }
 
@@ -54,7 +58,7 @@ class User extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'nick' => 'Nick',
+            'nick' => '微信昵称',
             'avator' => 'Avator',
             'name' => 'Name',
             'sex' => 'Sex',
@@ -62,10 +66,18 @@ class User extends \yii\db\ActiveRecord
             'city_id' => 'City ID',
             'open_id' => 'Open ID',
             'is_checked' => 'Is Checked',
-            'business_id' => 'Business ID',
-            'is_manager' => 'Is Manager',
-            'id_card' => 'Id Card',
-            'rid' => 'Rid',
+            'business_id' => '所属车商',
+            'is_manager' => '是否管理员',
+            'id_card' => '身份证号',
+            'rid' => '推荐人ID',
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBusiness()
+    {
+        return $this->hasOne(Business::className(), ['id' => 'business_id']);
     }
 }
