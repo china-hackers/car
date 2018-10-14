cdcb8437b98363272458f9da2d981a86d83220a1
Leonidax <wap@iamlk.cn>
2018-10-14 18:53:31 +0800

    提交新接口

--- controllers/Mobile/SiteController.php
+++ controllers/Mobile/SiteController.php
@@ -3,15 +3,30 @@
 namespace app\controllers\mobile;
 
 use app\controllers\base\BaseController;
+use app\models\UploadModel;
+use yii\web\UploadedFile;
+use yii;
 
 class SiteController extends BaseController
 {
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
+
+
+    public function actionSafeimg(){
+        $this->layout = false;
+        $model = new UploadModel();
+        if (Yii::$app->request->isPost) {
+            $model->images = UploadedFile::getInstances($model, 'images');
+            $list = $model->upload('safe');
+            if (count($list)) {
+                $this->data['data'] = $list;
+                return $this->json();
+            }else{
+                return $this->json(401,'上传图片失败');
+            }
+        }
+    }
+
     public function actionIndex()
     {
         $this->layout = false;
