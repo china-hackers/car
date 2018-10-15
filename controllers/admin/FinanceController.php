9b0d435ea55df677e21e6a1ec2ce47ddecfa6da9
Leonidax <wap@iamlk.cn>
2018-10-15 19:26:22 +0800

    添加相关接口

--- controllers/admin/FinanceController.php
+++ controllers/admin/FinanceController.php
@@ -3,6 +3,8 @@
 namespace app\controllers\admin;
 
 use app\controllers\base\AController;
+use app\models\IBuy;
+use app\models\IBuyLog;
 use app\models\ILoan;
 use app\models\ISafe;
 use app\models\ILoanLog;
@@ -10,6 +12,7 @@ use app\models\ISafeLog;
 
 class FinanceController extends AController
 {
+
 
 
     public function actionSafelist(){
@@ -96,6 +99,49 @@ class FinanceController extends AController
         }else{
             return $this->error($model);
         }
+    }
+
+    public function actionBuycheck(){
+        if(empty($this->post['id'])) return $this->json(402,'ID不能为空');
+        $model = IBuy::findOne($this->post['id']);
+        if($model){
+            $model->is_deal = 1;
+            $model->save();
+            return $this->json();
+        }else{
+            return $this->json(402,'没有找到该记录');
+        }
+    }
+
+    public function actionBuylog(){
+        $model = new IBuyLog();
+        $model->attributes = $this->post;
+        $model->created = time();
+        if($model->save()){
+            return $this->json();
+        }else{
+            return $this->error($model);
+        }
+    }
+
+    public function actionBuylist(){
+        $p = @$this->post['p']?$this->post['p']:1;
+        if(@$this->post['name']) {
+            $count = IBuy::find()->where('name like "%' . $this->post['name'] . '%"')->count();
+            $list = IBuy::find()->where('name like "%' . $this->post['name'] . '%"')->offset(($p - 1) * 20)->limit(20)->all();
+        }else{
+            $count = IBuy::find()->count();
+            $list = IBuy::find()->offset(($p-1)*20)->limit(20)->all();
+        }
+        $data = ['total'=>intval($count)];
+        $l2 = [];
+        foreach($list as $li){
+            $tmp = $li->attributes;
+            $l2[] = $tmp;
+        }
+        $data['list'] = $l2;
+        $this->data['data'] = $data;
+        return $this->json();
     }
 
 }
