855494e4e6681f1e895270a59002d4052666eaed
Leonidax <wap@iamlk.cn>
2018-10-23 20:13:44 +0800

    新街口

--- controllers/mobile/UserController.php
+++ controllers/mobile/UserController.php
@@ -10,6 +10,7 @@ namespace app\controllers\mobile;
 
 use app\controllers\base\MController;
 use abei2017\wx\Application;
+use app\models\User;
 
 class UserController extends MController{
 
@@ -17,6 +18,20 @@ class UserController extends MController{
         $qrcode = (new Application())->driver("mp.qrcode");
         $r = $qrcode->forever(@$this->post['id']);
         $this->data['data']['qrcode'] = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='.$r['ticket'];
+        return $this->json();
+    }
+
+    public function actionUser(){
+        $model = User::findOne($this->post['id']);
+        if(!$model) return $this->json(404,'该用户不存在');
+        $this->data['data'] = $model->attributes;
+        if($model->user){
+            $user = [];
+            $user['name'] = $model->user->name;
+            $user['phone'] = $model->user->phone;
+            $this->data['data']['user'] = $user;
+        }
+        if($model->car) $this->data['data']['car'] = $model->car->attributes;
         return $this->json();
     }
 }
