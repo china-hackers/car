3ee8e9fb8baa04722916816fc1f320a559cc2e81
Leonidax <wap@iamlk.cn>
2018-11-21 15:18:29 +0800

    test

--- web/static/admin/js/18.317a86380d608d22c994.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([18],{Ni1S:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"page-welcome"},[this._v("欢迎光临")])},staticRenderFns:[]},s=l("7jd7")(null,n,!1,null,null,null);t.default=s.exports}});
-//# sourceMappingURL=18.317a86380d608d22c994.js.map
